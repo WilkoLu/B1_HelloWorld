4ce5b57bb758890a03af3915cd015c2527726ce4
FelixM1995 <56156452+FelixM1995@users.noreply.github.com>
2019-10-04 13:26:40 +0200

    Update Hello world.cs

--- Hello world.cs
+++ Hello world.cs
@@ -14,7 +14,7 @@ namespace Hello_World_Mecke
         static void Main(string[] args) // Hier beginnt das Hauptprogramm
         {
             // Ausgabe Benutzer 1
-            Console.WriteLine("Hello World von XXX");
+            Console.WriteLine("Hello World von 123");
 
             // Ausgabe Benutzer 2
             Console.WriteLine("Nǐ hǎo, shìjiè");
