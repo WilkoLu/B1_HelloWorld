129ae971712013e54446ee03123acd322f37dc85
Wilko Lühring <56156137+WilkoLu@users.noreply.github.com>
2019-10-10 13:17:30 +0200

    Add files via upload

---
+++ Profilrechner/Profilrechner/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profilrechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Profilrechner Rechteckprofil");
+            Console.WriteLine();
+            Console.WriteLine("Querschnittsberechnung");
+            Console.WriteLine();
+            Console.WriteLine("Eingabe Breite in mm");
+            string eingabebreite = Console.ReadLine();
+            double breite = Convert.ToDouble(eingabebreite);
+            Console.WriteLine("Eingabe Hoehe in mm");
+            string eingabehoehe = Console.ReadLine();
+            double hoehe = Convert.ToDouble(eingabehoehe);
+            double qflaeche = breite * hoehe;
+            Console.WriteLine("Querschnittsflaeche");
+            Console.Write(qflaeche);
+            Console.WriteLine("mm²");
+            Console.ReadKey();
+
+        }
+    }
+}
