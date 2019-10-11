9d86f42f42b6462dc672d33d0f3bb8673efc0dee
WilkoLu <56156137+WilkoLu@users.noreply.github.com>
2019-10-11 14:43:30 +0200

    Neues Update mit Fehlerbetrachtung

--- Profilrechner/Profilrechner/Program.cs
+++ Profilrechner/Profilrechner/Program.cs
@@ -10,21 +10,45 @@ namespace Profilrechner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Profilrechner Rechteckprofil");
             Console.WriteLine();
-            Console.WriteLine("Querschnittsberechnung:");
+            Console.WriteLine("             Profilrechner Rechteckprofil");
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine("                 Querschnittsflaeche");
+            Console.WriteLine("------------------------------------------------------");
             Console.WriteLine();
-            Console.Write("Eingabe Breite in mm: ");
+            Console.Write("   Eingabe Breite in mm: ");
             string eingabebreite = Console.ReadLine();
-            double breite = Convert.ToDouble(eingabebreite);
-            Console.Write("Eingabe Hoehe in mm: ");
-            string eingabehoehe = Console.ReadLine();
+            double breite;
+            bool echtebreite = double.TryParse(eingabebreite, out breite);
+            if(echtebreite == false)
+                {
+                Console.WriteLine();
+                Console.WriteLine("     Fehler in der Matrix!");
+                }
             Console.WriteLine();
-            double hoehe = Convert.ToDouble(eingabehoehe);
+            Console.Write("   Eingabe Hoehe in mm: ");
+            string eingabehoehe = Console.ReadLine();
+            double hoehe;
+            bool echtehoehe = double.TryParse(eingabehoehe, out hoehe);
+            if(echtehoehe == false)
+                {
+                Console.WriteLine();
+                Console.WriteLine("     Fehler in der Matrix!");
+                }
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------------------------");
             double qflaeche = breite * hoehe;
-            Console.Write("Querschnittsflaeche = ");
-            Console.Write(qflaeche);
-            Console.WriteLine("mm²");
+            Console.Write("   Querschnittsflaeche = ");
+            if (qflaeche <= 0)
+                {
+                Console.WriteLine ("Wert nicht real.");
+                }
+            else 
+                    {
+                Console.Write (qflaeche);
+                Console.WriteLine ("mm²");
+                    }
+            Console.WriteLine("------------------------------------------------------");
             Console.WriteLine();
             Console.WriteLine("Press any key to close");
             Console.ReadKey();
