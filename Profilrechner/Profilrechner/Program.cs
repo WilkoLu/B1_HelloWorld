e1b63620c1e1a9a49907b5dc00cbca3fe2171847
WilkoLu <56156137+WilkoLu@users.noreply.github.com>
2019-10-16 13:34:16 +0200

    Volumen Felix 16.10.2019

--- Profilrechner/Profilrechner/Program.cs
+++ Profilrechner/Profilrechner/Program.cs
@@ -18,7 +18,7 @@ namespace Profilrechner
                 Console.WriteLine();
                 Console.WriteLine("                       Profilrechner");
                 Console.WriteLine("----------------------------------------------------------");
-                Console.WriteLine("           Welches Profil wollen sie berechnen?");
+                Console.WriteLine("          Welches Profil wollen sie berechnen?");
                 Console.WriteLine();
                 Console.WriteLine(" < 1 > Rechteckprofil");
                 Console.WriteLine();
@@ -38,7 +38,7 @@ namespace Profilrechner
                 }
                 else
                 {
-                    Console.WriteLine("Fehler bei der Eingabe");
+                    Console.WriteLine(" Fehler bei der Eingabe!");
                 }
 
             } while (menue != 0);
@@ -57,6 +57,7 @@ namespace Profilrechner
                 Console.WriteLine();
                 Console.WriteLine(" < 1 > Querschnittsflaeche");
                 Console.WriteLine(" < 2 > Flaechentraegheitsmoment");
+                Console.WriteLine(" < 3 > Volumen");
                 Console.WriteLine();
                 Console.WriteLine("----------------------------------------------------------");
                 Console.WriteLine(" Zum beenden des Programms drücken Sie 0");
@@ -71,6 +72,12 @@ namespace Profilrechner
                 else if (menue == 2)
                 {
                     p.Flaechentraegheitsmoment();
+                    menue = 0;
+                }
+
+                else if (menue == 3)
+                {
+                    p.Volumen();
                     menue = 0;
                 }
                 else
@@ -98,9 +105,9 @@ namespace Profilrechner
             Console.WriteLine("----------------------------------------------------------");
             Console.WriteLine();
 
-            breite = p.EingabeMitPrüfungQuerschnitt("Breite");
+            breite = p.EingabeMitPrüfungQuerschnittVolumen("Breite");
 
-            hoehe = p.EingabeMitPrüfungQuerschnitt("Hoehe");
+            hoehe = p.EingabeMitPrüfungQuerschnittVolumen("Hoehe");
 
             qflaeche = breite * hoehe;
             if (breite < 0 & hoehe < 0)
@@ -162,8 +169,8 @@ namespace Profilrechner
         BeginFlaechentraegheitsmoment:
 
             Program p = new Program();
-            double flaechenmomentX ;
-            double flaechenmomentY ;
+            double flaechenmomentX;
+            double flaechenmomentY;
             double breite;
             double hoehe;
 
@@ -178,7 +185,7 @@ namespace Profilrechner
             breite = p.EingabeMitPrüfung("Breite");
 
             hoehe = p.EingabeMitPrüfung("Hoehe");
-           
+
 
             flaechenmomentX = breite * hoehe * hoehe * hoehe / 12;
             flaechenmomentY = hoehe * breite * breite * breite / 12;
@@ -216,31 +223,113 @@ namespace Profilrechner
         End:;
         }
 
+        void Volumen()
+        {
+
+
+        BeginVolumen:
+            Console.Clear();
+            Program p = new Program();
+            double breite;
+            double laenge;
+            double volumen;
+            double qflaeche;
+            double hoehe;
+
+            Console.WriteLine();
+            Console.WriteLine("                      Rechteckprofil");
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("                         Volumen");
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine();
+         
+            
+            breite = p.EingabeMitPrüfungQuerschnittVolumen("Breite");
+            hoehe = p.EingabeMitPrüfungQuerschnittVolumen("Höhe");
+            laenge = p.EingabeMitPrüfungQuerschnittVolumen("Länge");
+
+            qflaeche = hoehe * breite;
+            volumen = qflaeche * laenge;
+
+            if (qflaeche < 0 & laenge < 0)
+            {
+                Console.Write(" Querschnitt und Länge sind negativ. Trotzdem darstellen? J/N:");       // Falls beide Werte negativ sind kommt ein positives Ergebnis raus.
+                ConsoleKeyInfo eingabekey = Console.ReadKey();                                   // Ist das der Fall kann der Benutzer sich entscheiden ob es weitergeht.
+                Console.WriteLine();
+                if (eingabekey.Key == ConsoleKey.N)
+                {
+                    Console.WriteLine();
+                    goto Finish;
+                }
+            }
+            if (volumen <= 0)                                                                    // Prüfung ob Volumen real ist. Volumen sind immer Positiv und nicht null.
+            {
+                Console.WriteLine("----------------------------------------------------------");
+                Console.WriteLine("                  Volumen nicht real.");
+            }
+            else
+            {
+                Console.WriteLine("----------------------------------------------------------");
+                Console.WriteLine();
+                Console.Write("   Volumen: ");
+                Console.Write(volumen);
+                Console.WriteLine("mm³");
+                Console.WriteLine();
+            }
+        Finish:
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine(" Press B to go back to the main Menu");
+            Console.WriteLine(" Press R to calculate again");
+            Console.WriteLine(" Press ESC to exit Program");
+        Returnbefehl:
+            ConsoleKeyInfo zurueck = Console.ReadKey();                                            // Tastendruck um neu zu starten. Zu Schließen oder zurück zu gehen.
+            if (zurueck.Key == ConsoleKey.B)
+            {
+                Console.Clear();
+                goto End;
+            }
+            else if (zurueck.Key == ConsoleKey.Escape)
+            {
+                Environment.Exit(0);
+            }
+            else if (zurueck.Key == ConsoleKey.R)
+            {
+                Console.Clear();
+                goto BeginVolumen;
+            }
+            else
+            {
+                goto Returnbefehl;
+            }
+        End:;
+
+        }
         double EingabeMitPrüfung(string definition)
         {
             Program p = new Program();
             double zahl = 0;
             bool echteeingabe = false;
-            
+
             while (echteeingabe == false)
             {
 
-                Console.Write("   Eingabe {0} : ",definition);
+                Console.Write("   Eingabe {0} : ", definition);
                 string eingabe = Console.ReadLine();
                 echteeingabe = double.TryParse(eingabe, out zahl);                                   //True wenn Zahl eingegeben wird False wenn nicht.
-                if (echteeingabe == false )                                                          //Prüfung ob Zahl eingegeben wurde oder die Zahl kleiner gleich 0 ist
-                   {
+                if (echteeingabe == false)                                                          //Prüfung ob Zahl eingegeben wurde oder die Zahl kleiner gleich 0 ist
+                {
                     Console.WriteLine();
                     Console.WriteLine("----------------------------------------------------------");
                     Console.WriteLine("                  Fehler bei der Eingabe!");
                     Console.WriteLine("----------------------------------------------------------");
                     Console.WriteLine();
-                   }
+                }
                 else if (zahl <= 0)
                 {
                     Console.WriteLine();
                     Console.WriteLine("----------------------------------------------------------");
-                    Console.WriteLine("Fehler bei der Eingabe! (Negative Zahl oder null kann nicht verwendet werden)!");    
+                    Console.WriteLine(" Fehler bei der Eingabe! (Negative Zahl oder null kann nicht verwendet werden)!");
                     Console.WriteLine("----------------------------------------------------------");
                     Console.WriteLine();
                     echteeingabe = false;                                                 // um bei negativen zahlen in der schleife zu bleiben, damit die eingebae erneut abgefragt wird
@@ -253,7 +342,7 @@ namespace Profilrechner
                         echteeingabe = false;
                     }
                 }
-                
+
 
             }
 
@@ -265,76 +354,72 @@ namespace Profilrechner
         {
             string einheit /*= "einheit"*/;
 
-              Console.Write("   Eingabe Einheit: ");
-              einheit = Console.ReadLine();
-              Console.WriteLine();
+            Console.Write("   Eingabe Einheit: ");
+            einheit = Console.ReadLine();
+            Console.WriteLine();
 
-              if (string.Compare(einheit, "mm") == 0)
-              {
-                  //es wird in mm gerechnet, also keine umwandlung
-              }
-              else if (string.Compare(einheit, "cm") == 0)
-              {
-                  zahl = zahl * 10;
-              }
-              else if (string.Compare(einheit, "dm") == 0)
-              {
-                  zahl = zahl * 100;
-              }
-              else if (string.Compare(einheit, "m") == 0)
-              {
-                  zahl = zahl * 1000;
-              }
-              else
-              {
-                  Console.WriteLine("----------------------------------------------------------");
-                  Console.WriteLine("Diese Einheit wird nicht unterstützt, bitte andere Wählen!");
-                  Console.WriteLine("----------------------------------------------------------");
-                  Console.WriteLine();
-                  zahl = -1;
-              }
+            if (string.Compare(einheit, "mm") == 0)
+            {
+                //es wird in mm gerechnet, also keine umwandlung
+            }
+            else if (string.Compare(einheit, "cm") == 0)
+            {
+                zahl = zahl * 10;
+            }
+            else if (string.Compare(einheit, "dm") == 0)
+            {
+                zahl = zahl * 100;
+            }
+            else if (string.Compare(einheit, "m") == 0)
+            {
+                zahl = zahl * 1000;
+            }
+            else
+            {
+                Console.WriteLine("----------------------------------------------------------");
+                Console.WriteLine(" Diese Einheit wird nicht unterstützt, bitte andere Wählen!");
+                Console.WriteLine("----------------------------------------------------------");
+                Console.WriteLine();
+                zahl = -69;
+            }
 
-            
+
             return zahl;
         }
 
-        double EingabeMitPrüfungQuerschnitt(string definition)
+        double EingabeMitPrüfungQuerschnittVolumen(string definition)
         {
             Program p = new Program();
             double zahl = 0;
             bool echteeingabe = false;
-            
+
             while (echteeingabe == false)
             {
 
-                Console.Write("   Eingabe {0} : ",definition);
+                Console.Write("   Eingabe {0} : ", definition);
                 string eingabe = Console.ReadLine();
                 echteeingabe = double.TryParse(eingabe, out zahl);                                   //True wenn Zahl eingegeben wird False wenn nicht.
-                if (echteeingabe == false )                                                          //Prüfung ob Zahl eingegeben wurde oder die Zahl kleiner gleich 0 ist
-                   {
+                if (echteeingabe == false)                                                          //Prüfung ob Zahl eingegeben wurde oder die Zahl kleiner gleich 0 ist
+                {
                     Console.WriteLine();
                     Console.WriteLine("----------------------------------------------------------");
                     Console.WriteLine("                  Fehler bei der Eingabe!");
                     Console.WriteLine("----------------------------------------------------------");
                     Console.WriteLine();
-                   }
+                }
                 else
                 {
                     zahl = p.Einheit(zahl);
-                    if (zahl == -1)                                                                    //wenn nicht unterstützte einheit eingegeben wurde
+                    if (zahl == -69)                                                                    //wenn nicht unterstützte einheit eingegeben wurde
                     {
                         echteeingabe = false;
                     }
                 }
-                
+
 
             }
 
             return zahl;
         }
-
-
-
-
     }
 }
