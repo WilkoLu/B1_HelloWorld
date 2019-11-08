a5d7c9007229c34bf6649db6b4716d64ac0fa494
EikeJoo <56156680+EikeJoo@users.noreply.github.com>
2019-11-08 01:28:21 +0100

    rumprobiert

--- Profilrechner/Profilrechner/MainWindow.xaml.cs
+++ Profilrechner/Profilrechner/MainWindow.xaml.cs
@@ -23,17 +23,57 @@ namespace Profilrechner
         public MainWindow()
         {
             InitializeComponent();
+            this.Hide();
+
+            System.Windows.Forms.NotifyIcon notifyIcon = new System.Windows.Forms.NotifyIcon();
+            notifyIcon.Icon = new System.Drawing.Icon("icon.ico");
+            notifyIcon.Visible = true;
+
+            notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(notifyIcon_click);
+
+            notifyIcon.ShowBalloonTip(400, "Profilrechner", "Erfolgreich gestartet", System.Windows.Forms.ToolTipIcon.Info);
+
+            System.Windows.Forms.ContextMenu notifyIconContextMenue = new System.Windows.Forms.ContextMenu();
+            notifyIconContextMenue.MenuItems.Add("Rechteckprofil", new EventHandler(f55));
+            notifyIconContextMenue.MenuItems.Add("profil2", new EventHandler(f55));
+            notifyIconContextMenue.MenuItems.Add("Rechteckprofil6", new EventHandler(f55));
+            notifyIconContextMenue.MenuItems.Add("Rechteckprofil345345", new EventHandler(f55));
+            notifyIconContextMenue.MenuItems.Add("Beenden", new EventHandler(Beenden));
+            
+
+            notifyIcon.ContextMenu = notifyIconContextMenue;
+
         }
 
         private void Rechteckprofil_Click(object sender, RoutedEventArgs e)
         {
             Rechteckprofil wnd = new Rechteckprofil();
             wnd.Show();
+            this.Hide();
         }
 
         private void Rechteckrohr_Click(object sender, RoutedEventArgs e)
         {
-
+            this.Hide();
         }
+        private void f55(object sender, EventArgs e)
+        {
+            Rechteckprofil wnd = new Rechteckprofil();
+            wnd.Show();
+        }
+        private void Beenden(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void notifyIcon_click(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if(e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                this.Show();
+            }
+        }
+
+        
     }
 }
--- Profilrechner/Profilrechner/Rechteckprofil.xaml.cs
+++ Profilrechner/Profilrechner/Rechteckprofil.xaml.cs
@@ -30,7 +30,6 @@ namespace Profilrechner
         private void Berechnen(object sender, RoutedEventArgs e)
         {
             
-            
             double qflaeche = 0;
             double volumen;
             double masse;
@@ -39,103 +38,31 @@ namespace Profilrechner
             double breite = 0;
             double flaechenträgheitsmomentX;
             double flaechenträgheitsmomentY;
+            
 
-
-            hoehe = eingabeMitPruefung(eingabeHoehe.Text, einheitHoehe.Text);
-            breite = eingabeMitPruefung(eingabeBreite.Text, einheitBreite.Text);
-            laenge = eingabeMitPruefung(eingabeLaenge.Text, einheitLaenge.Text);
+            hoehe = eingabeMitEinheit.eingabeMitPruefung(eingabeHoehe.Text, einheitHoehe.Text);
+            breite = eingabeMitEinheit.eingabeMitPruefung(eingabeBreite.Text, einheitBreite.Text);
+            laenge = eingabeMitEinheit.eingabeMitPruefung(eingabeLaenge.Text, einheitLaenge.Text);
 
             qflaeche = hoehe * breite;
-            lbl_qflaeche.Content = qflaeche + " mm²";
-            flaechenträgheitsmomentX = breite * Math.Pow(hoehe, 3) / 12;
-            lbl_flaechentraegheitsmomentX.Content = flaechenträgheitsmomentX + " mm⁴";
+            lbl_qflaeche.Content = Math.Round(qflaeche ,3)  + " mm²" ; 
+             flaechenträgheitsmomentX = breite * Math.Pow(hoehe, 3) / 12;
+            lbl_flaechentraegheitsmomentX.Content = Math.Round(flaechenträgheitsmomentX , 3) + " mm⁴";
             flaechenträgheitsmomentY = hoehe * Math.Pow(breite, 3) / 12;
-            lbl_flaechentraegheitsmomentY.Content = flaechenträgheitsmomentY + " mm⁴";
+            lbl_flaechentraegheitsmomentY.Content = Math.Round(flaechenträgheitsmomentY , 3) + " mm⁴";
             volumen = laenge * qflaeche;
-            lbl_volumen.Content = volumen + " mm³";
+            lbl_volumen.Content = Math.Round(volumen , 3) + " mm³";
 
-            if (rb_stahl.IsChecked == true)
-            {
-                masse = volumen * 7.85 * Math.Pow(10, -6);
-                lbl_masse.Content = masse + " kg";
-            }
-            else if (rb_aluminium.IsChecked == true)
-            {
-                masse = volumen * 2.7 * Math.Pow(10, -6);
-                lbl_masse.Content = masse + " kg";
-            }
-
-
-        }
-
-        public static double eingabeMitPruefung(string zahl, string einheit)
-        {
-            double rueckgabewert;
-
-            string eingabe = zahl;
-            bool zahlOderNicht = double.TryParse(eingabe, out rueckgabewert);
-            if (zahlOderNicht == true)
-            {
-               rueckgabewert = Einheitenrechner(rueckgabewert, einheit);
-            }
+            masse = volumen * Material.dichte(cb_Material.Text);
+            lbl_masse.Content = Math.Round(masse , 3) + " kg";
             
-            if (rueckgabewert == -1)
-            {
-                MessageBox.Show("Die Einheit " + einheit + " wird nicht unterstützt! Bitte wählen sie eine andere.","Einheitenfehler",
-                    MessageBoxButton.OK,
-                    MessageBoxImage.Warning
-                    );
-                return 0;
-            }
-            else
-            {
-                return rueckgabewert;
-            }
 
         }
 
-        public static double Einheitenrechner(double zahl,string einheit)
+        private void zuruek_Click(object sender, RoutedEventArgs e)
         {
-
-            if (einheit.Equals("mm"))
-            {
-                //es wird in mm gerechnet, also keine umwandlung
-            }
-            else if (einheit.Equals("cm"))
-            {
-                zahl = zahl * 10;
-            }
-            else if (einheit.Equals("dm"))
-            {
-                zahl = zahl * 100;
-            }
-            else if (einheit.Equals("m"))
-            {
-                zahl = zahl * 1000;
-            }
-            else if (einheit.Equals("km"))
-            {
-                zahl = zahl * 1000000;
-            }
-            else if (einheit.Equals("inch") || einheit.Equals("Inch") || einheit.Equals("zoll") || einheit.Equals("Zoll"))
-            {
-                zahl = zahl * 25.4;
-            }
-            else if (einheit.Equals("ft"))
-            {
-                zahl = zahl * 304.8;
-            }
-            else
-            {
-                zahl = -1;
-            }
-
-
-            return zahl;
+            this.Close();
+            //MainWindow.Show();
         }
-
-
-
-
     }
 }
