91faf8a1a91c13268a03cc37cae65e8706e2d790
EikeJoo <56156680+EikeJoo@users.noreply.github.com>
2019-11-08 17:02:44 +0100

    weiter rumprobiert

---
+++ Profilrechner/Profilrechner/MainProgramm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows;
+
+
+
+namespace Profilrechner
+{
+
+    class Program
+    {
+        [STAThread]
+        static void Main (string[] args)
+        {
+            new Program();
+        }
+
+
+        public Program()
+        {
+            Window fenster = new Window();
+            Menue hauptfenster = new Menue();
+            
+            fenster.Content = hauptfenster.Content;
+            
+            fenster.ShowDialog();
+        }
+    }
+}
---
+++ Profilrechner/Profilrechner/menue.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Profilrechner
+{
+    /// <summary>
+    /// Interaktionslogik für Window1.xaml
+    /// </summary>
+    public partial class Menue : Window
+    {
+        public Menue()
+        {
+            InitializeComponent();
+        }
+        private void Rechteckprofil_Click(object sender, RoutedEventArgs e)
+        {
+            Rechteckprofil wnd = new Rechteckprofil();
+            wnd.Show();
+            this.Hide();
+            
+        }
+
+        private void Rechteckrohr_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
